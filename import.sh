562c68b63d555d0c32cb78eb1dd9d76e4191ade4
Doron Behar <doron.behar@gmail.com>
2016-04-09 18:14:58 +0300

    add import option for ./import.sh
    ./import.sh <branch-name> imports only files associated with that branch

--- import.sh
+++ import.sh
@@ -1,23 +1,53 @@
-# shells:
-rsync ~/.profile ./.profile
-rsync ~/.bashrc ./.bashrc
-rsync ~/.config/powerline/ ./.config/powerline/
-rsync ~/.zprofile ./.zprofile
-rsync ~/.zlogin ./.zlogin
-rsync ~/.zshenv ./.zshenv
-rsync ~/.zshrc ./.zshrc
-rsync -av --delete ~/.sh/ ./.sh/
-rsync -av --delete ~/.bin/ ./.bin/
-# git:
-rsync ~/.gitconfig ./.gitconfig
-# vim:
-rsync -av --delete ~/.vim/ ./.vim/
-rsync ~/.vimrc ./
-# tmux:
-rsync -av --delete ~/.tmux/ ./.tmux/
-rsync ~/.tmux.conf ./.tmux.conf
-# ncmpcpp
-rsync -av --delete ~/.ncmpcpp/ ./.ncmpcpp/
-# print the status:
-git status
+_shells(){
+	rsync ~/.profile ./.profile
+	rsync ~/.bashrc ./.bashrc
+	rsync ~/.config/powerline/ ./.config/powerline/
+	rsync ~/.zprofile ./.zprofile
+	rsync ~/.zlogin ./.zlogin
+	rsync ~/.zshenv ./.zshenv
+	rsync ~/.zshrc ./.zshrc
+	rsync -av --delete ~/.sh/ ./.sh/
+	rsync -av --delete ~/.bin/ ./.bin/
+}
+_git(){
+	rsync ~/.gitconfig ./.gitconfig
+}
+_vim(){
+	rsync -av --delete ~/.vim/ ./.vim/
+	rsync ~/.vimrc ./
+}
+_tmux(){
+	rsync -av --delete ~/.tmux/ ./.tmux/
+	rsync ~/.tmux.conf ./.tmux.conf
+}
+_ncmpcpp(){
+	rsync -av --delete ~/.ncmpcpp/ ./.ncmpcpp/
+}
+
+case "$1" in
+	shells)
+		_shells
+		;;
+	git)
+		_git
+		;;
+	vim)
+		_vim
+		;;
+	tmux)
+		_tmux
+		;;
+	ncmpcpp)
+		_ncmpcpp
+		;;
+	*)
+		_ncmpcpp
+		_tmux
+		_vim
+		_git
+		_shells
+		# print the status:
+		git status
+		;;
+esac
 # vim: ft=sh
