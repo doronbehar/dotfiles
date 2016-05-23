c4fafac5cab2f84593b15a86994c0bda155e6993
Doron Behar <doron.behar@gmail.com>
2016-05-23 03:12:27 +0300

    Add awesome files.
    Use better keyboard bindings.
    Copy theme files from /usr/share/awesome/themes/default.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -43,7 +43,7 @@ require("debian.menu")
 
 -- {{{ Variable definitions
 	-- Themes define colours, icons, font and wallpapers.
-	beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+	beautiful.init("~/.config/awesome/theme.lua")
 
 	-- This is used later as the default terminal and editor to run.
 	terminal = "xterm"
@@ -195,7 +195,7 @@ require("debian.menu")
 		mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
 		-- Create the wibox
-		mywibox[s] = awful.wibox({ position = "top", screen = s })
+		mywibox[s] = awful.wibox({ position = "top", height ="20", screen = s })
 
 		-- Widgets that are aligned to the left
 		local left_layout = wibox.layout.fixed.horizontal()
@@ -230,51 +230,52 @@ require("debian.menu")
 -- {{{ Key bindings
 	globalkeys = awful.util.table.join(
 
-		awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-		awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-		awful.key({ modkey,           },"j",
+		awful.key({modkey,"Mod1"		}, "j",			awful.tag.viewnext),
+		awful.key({modkey,"Mod1"		}, "k",			awful.tag.viewprev),
+		awful.key({modkey,				}, "j",
 			function ()
 				awful.client.focus.byidx( 1)
 				if client.focus then client.focus:raise() end
 			end),
-		awful.key({ modkey,           }, "k",
+		awful.key({modkey,				}, "k",
 			function ()
 				awful.client.focus.byidx(-1)
 				if client.focus then client.focus:raise() end
 			end),
-		awful.key({ modkey,           }, "w", function () mymainmenu:show() end),
+		awful.key({modkey,				}, "s",			function () mymainmenu:show() end),
 
 	-- Layout manipulation
-		awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-		awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-		awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-		awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
-		awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
-		awful.key({ modkey,           }, "Tab",
-			function ()
-				awful.client.focus.history.previous()
-				if client.focus then
-					client.focus:raise()
-				end
-			end),
+		awful.key({modkey,				}, "Up",		function () awful.client.swap.byidx(  1)    end),
+		awful.key({modkey,				}, "Down",		function () awful.client.swap.byidx( -1)    end),
+		awful.key({modkey,				}, "Tab",		function () awful.screen.focus_relative( 1) end),
 
 	-- Standard program
-		awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-		awful.key({ modkey, "Control" }, "r", awesome.restart),
-		awful.key({ modkey, "Escape"   }, "q", awesome.quit),
-		awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05) end),
-		awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05) end),
-		awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)  end),
-		awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)  end),
-		awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)   end),
-		awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)   end),
-		awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts, 1) end),
-		awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts,-1) end),
-		awful.key({ modkey, "Control" }, "n", awful.client.restore),
+		awful.key({modkey,				}, "Return",	function () awful.util.spawn(terminal) end),
+		awful.key({"Control","Mod1"		}, "t",			function () awful.util.spawn("xterm -e \"tmux attach-session -t project || tmux\"") end),
+		awful.key({"Control","Mod1"		}, "w",			function () awful.util.spawn("google-chrome") end),
+		awful.key({"Control","Mod1"		}, "n",			function () awful.util.spawn("nautilus") end),
+		awful.key({"Control","Mod1"		}, "p",			function () awful.util.spawn("xterm -e ncmpcpp") end),
+		awful.key({"Control","Mod1"		}, "e",			function () awful.util.spawn("xterm -e \"cd repos/dotfiles/.config/awesome; vim rc.lua\"") end),
+		awful.key({modkey,"Mod1"		}, "r",			awesome.restart),
+		awful.key({modkey,"Mod1"		}, "q",			awesome.quit),
+		awful.key({modkey,				}, "Right",		function () awful.tag.incmwfact( 0.02) end),
+		awful.key({modkey,				}, "Left",		function () awful.tag.incmwfact(-0.02) end),
+		awful.key({modkey,"Shift"		}, "Right",		function () awful.tag.incnmaster( 1)  end),
+		awful.key({modkey,"Shift"		}, "Left",		function () awful.tag.incnmaster(-1)  end),
+		awful.key({modkey,"Control"		}, "Right",		function () awful.tag.incncol( 1)   end),
+		awful.key({modkey,"Control"		}, "Left",		function () awful.tag.incncol(-1)   end),
+		awful.key({modkey,				}, "space",		function () awful.layout.inc(layouts, 1) end),
+		awful.key({modkey,"Shift"		}, "space",		function () awful.layout.inc(layouts,-1) end),
+	-- Music Player:
+		awful.key({						}, "Pause",		function () awful.util.spawn("mpc toggle") end),
+		awful.key({						}, "F9",		function () awful.util.spawn("mpc next") end),
+		awful.key({						}, "F8",		function () awful.util.spawn("mpc prev") end),
+		awful.key({"Control"			}, "F12",		function () awful.util.spawn("mpc seek +5") end),
+		awful.key({"Control"			}, "F11",		function () awful.util.spawn("mpc seek -5") end),
 
 	-- Prompt
-		awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run()end),
-		awful.key({ modkey }, "x",
+		awful.key({modkey				}, "r",			function () mypromptbox[mouse.screen]:run()end),
+		awful.key({modkey				}, "x",
 			function ()
 				awful.prompt.run({ prompt = "Run Lua code: " },
 				mypromptbox[mouse.screen].widget,
@@ -282,24 +283,18 @@ require("debian.menu")
 				awful.util.getdir("cache") .. "/history_eval")
 			end),
 	-- Menubar
-		awful.key({ modkey }, "p", function() menubar.show() end)
+		awful.key({modkey,"Mod1"		}, "m",			function () menubar.show() end)
 	)
-
 	clientkeys = awful.util.table.join(
-		awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-		awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-		awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
-		awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-		awful.key({ modkey,           }, "o",      function(c) awful.client.movetoscreen(c,c.screen-1) end ),
-		awful.key({ modkey,           }, "p",      function(c) awful.client.movetoscreen(c,c.screen+1) end ),
-		awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-		awful.key({ modkey,           }, "n",
-			function (c)
-				-- The client currently has the input focus, so it cannot be
-				-- minimized, since minimized clients can't have the focus.
-				c.minimized = true
-			end),
-		awful.key({ modkey,           }, "m",
+		awful.key({modkey,				}, "f",			function (c) c.fullscreen = not c.fullscreen  end),
+		awful.key({modkey,				}, "q",			function (c) c:kill()                         end),
+		awful.key({modkey,"Shift"		}, "q",			awful.client.restore),
+		awful.key({modkey,"Shift"		}, "space",		awful.client.floating.toggle                     ),
+		awful.key({modkey,"Control"		}, "Return",	function (c) c:swap(awful.client.getmaster()) end),
+		awful.key({modkey,				}, "u",			function (c) awful.client.movetoscreen(c,c.screen-1) end ),
+		awful.key({modkey,				}, "i",			function (c) awful.client.movetoscreen(c,c.screen+1) end ),
+		awful.key({modkey,				}, "t",			function (c) c.ontop = not c.ontop            end),
+		awful.key({modkey,				}, "m",
 			function (c)
 				c.maximized_horizontal = not c.maximized_horizontal
 				c.maximized_vertical   = not c.maximized_vertical
@@ -350,7 +345,6 @@ require("debian.menu")
 					end
 				end))
 	end
-
 	clientbuttons = awful.util.table.join(
 		awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
 		awful.button({ modkey }, 1, awful.mouse.client.move),
