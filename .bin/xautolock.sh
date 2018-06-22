127070d504c982350754df07bc5ea82a4a8f28b1
Doron Behar <doron.behar@gmail.com>
2018-06-22 18:02:41 +0300

    Reset xautolock notification to 5 seconds.

--- .bin/xautolock.sh
+++ .bin/xautolock.sh
@@ -3,5 +3,4 @@
 	-time 7 \
 	-locker "${HOME}/.bin/xlock.sh" \
 	-corners '-+-+' \
-	-notify '10' \
-	-notifier 'notify-send --app-name=xautolock --expire-time=10000 "xautolock will lock the computer in 10 seconds of inactivity"'
+	-notifier 'notify-send --app-name=xautolock --expire-time=10000 "xautolock will lock the computer in 5 seconds of inactivity"'
