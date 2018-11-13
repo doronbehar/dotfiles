487bd80539eb235b86bcd03b8c6755b5246567e2
Doron Behar <doron.behar@gmail.com>
2018-11-13 12:28:46 +0200

    Recursively manage submodules sets

--- submodules-sets/servers.sh
+++ submodules-sets/servers.sh
@@ -1,72 +1,32 @@
-git submodule init -- \
-	.config/git \
-	.config/pip \
-	.config/pulse \
-	.config/ranger \
-	.config/lf \
-	.cpan/CPAN \
-	.gnupg \
-	.ssh \
-	.tmux \
-	.zsh-completions \
-	.zsh-syntax-highlighting \
+#!/bin/sh
+
+update_all=(
+	.config/git
+	.config/pip
+	.config/pulse
+	.config/ranger
+	.config/lf
+	.cpan/CPAN
+	.gnupg
+	.ssh
+	.tmux
+	.zsh-completions
+	.zsh-syntax-highlighting
 	.zsh-system-clipboard
+)
+
+git submodule init -- $update_all
 git submodule update --recursive --checkout
 
-git submodule init -- .config/nvim
-git -C .config/nvim submodule init -- \
-	bundle/AnsiEsc \
-	bundle/SimpylFold \
-	bundle/abolish \
-	bundle/airline \
-	bundle/ale \
-	bundle/bbye \
-	bundle/bufexplorer \
-	bundle/css-color \
-	bundle/csv \
-	bundle/devicons \
-	bundle/dirdiff \
-	bundle/easymotion \
-	bundle/editorconfig \
-	bundle/eunuch \
-	bundle/highlitedyank \
-	bundle/info \
-	bundle/neomutt \
-	bundle/nerdcommenter \
-	bundle/nerdtree \
-	bundle/orgmode \
-	bundle/pandoc \
-	bundle/pandoc-syntax \
-	bundle/pathogen \
-	bundle/peekaboo \
-	bundle/push \
-	bundle/qf_resize \
-	bundle/repeat \
-	bundle/rooter \
-	bundle/sandwich \
-	bundle/searchindex \
-	bundle/signify \
-	bundle/sleuth \
-	bundle/speeddating \
-	bundle/stay \
-	bundle/suda \
-	bundle/tabular \
-	bundle/targets \
-	bundle/textobj-fold \
-	bundle/textobj-keyvalue \
-	bundle/textobj-quotes \
-	bundle/textobj-underscore \
-	bundle/textobj-uri \
-	bundle/textobj-user \
-	bundle/tmux-focus-events \
-	bundle/tmux-navigator \
-	bundle/tmux-syntax \
-	bundle/togglelist \
-	bundle/tradewinds \
-	bundle/transpose \
-	bundle/unicode \
-	bundle/visualrepeat \
-	bundle/window-swap \
-	bundle/fugitive \
-	bundle/nftables
-git -C .config/nvim submodule update --recursive --checkout
+update_selectively=(
+	.config/nvim
+)
+
+this_repo=$(cd $(dirname $0)/.. && pwd -P)
+
+for sm in $update_selectively; do
+	git submodule init -- $this_repo/$sm
+	cd $this_repo/$sm
+	./submodules-sets/$(basename $0)
+	cd $this_repo
+done
