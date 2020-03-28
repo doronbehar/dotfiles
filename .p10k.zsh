2664c500e488ce82925781e656de83ac0e9999da
Doron Behar <doron.behar@gmail.com>
2020-03-28 21:00:20 +0300

    Make p10k theme better for vim terminal

--- .p10k.zsh
+++ .p10k.zsh
@@ -217,7 +217,7 @@
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=6
   # Make the icon bold.
   typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='%B${P9K_CONTENT// }'
 
