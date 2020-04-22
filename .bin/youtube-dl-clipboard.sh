ca4e473cc28eb42d9c49a0442a9027f8b52af35e
Doron Behar <doron.behar@gmail.com>
2020-04-22 16:10:11 +0300

    youtube-dl-clipboard: UX - remember last save dir

--- .bin/youtube-dl-clipboard.sh
+++ .bin/youtube-dl-clipboard.sh
@@ -13,6 +13,15 @@ xsel -ob | \
 	zenity --width 2000 --list --title "Choose urls to download from" --column "URL" | \
 	while read url; do
 	cd ~/downloads
-	where_to_save="$(zenity --title="Select where to save the file" --file-selection --file-filter='*.mp4' --save --filename="youtube-dl-download.mp4")"
+	if [ -f ~/.cache/youtube-dl-clipboard/last_save_dir ]; then
+		last_save_dir=$(cat ~/.cache/youtube-dl-clipboard/last_save_dir)
+		if [ -d "$last_save_dir" ]; then
+			cd "$last_save_dir"
+		fi
+	fi
+	where_to_save="$(zenity --title="Select where to save the file" --file-selection --confirm-overwrite --file-filter='*.mp4' --save --filename="youtube-dl-download.mp4")"
+	location_dir="$(dirname "$where_to_save")"
+	mkdir -p ~/.cache/youtube-dl-clipboard
+	echo "$location_dir" > ~/.cache/youtube-dl-clipboard/last_save_dir
 	youtube-dl --output="$where_to_save" "$url"
 done
