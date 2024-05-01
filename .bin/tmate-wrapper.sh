3cfb8a70ecedb4dd53fca1970f3c9bb11f92dae1
Doron Behar <doron.behar@gmail.com>
2024-05-01 15:08:09 +0300

    Run tmate in a loop - to accommodate for unexpected errors

--- .bin/tmate-wrapper.sh
+++ .bin/tmate-wrapper.sh
@@ -2,4 +2,6 @@
 
 # ~/.tmate.session is stored in my ~/.ssh/tmate.conf as well - secretly - not git tracked
 # ~/.tmate-api.key is given by the web app.
-tmate -n $(cat ~/.tmate.session) -k $(cat ~/.tmate-api.key) -a ~/.ssh/authorized_keys
+while ! tmate -n $(cat ~/.tmate.session) -k $(cat ~/.tmate-api.key) -a ~/.ssh/authorized_keys; do
+	echo Restarting tmate, after an unexpected error
+done
