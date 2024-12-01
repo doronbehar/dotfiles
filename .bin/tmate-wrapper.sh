7d34b24ef5abcc997272f278348114c198a4222e
Doron Behar <doron.behar@gmail.com>
2024-12-01 13:54:31 +0200

    Replace tmux alias with something more conservative

--- .bin/tmate-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# ~/.tmate.session is stored in my ~/.ssh/tmate.conf as well - secretly - not git tracked
-# ~/.tmate-api.key is given by the web app.
-while ! tmate -n $(cat ~/.tmate.session) -k $(cat ~/.tmate-api.key) -a ~/.ssh/authorized_keys; do
-	echo Restarting tmate, after an unexpected error
-done
